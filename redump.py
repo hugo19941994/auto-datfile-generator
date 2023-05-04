deb239bf15bddb18f46bf7813290a4d67a09bde1
Daniel Tobias <dan.g.tob@gmail.com>
2023-05-04 17:52:16 +1000

    format redump.py with black

--- redump.py
+++ redump.py
@@ -6,16 +6,16 @@ from time import sleep
 import requests
 
 # Config
-URL_HOME      = 'http://redump.org/'
-URL_DOWNLOADS = 'http://redump.org/downloads/'
-XML_FILENAME  = 'redump.xml'
-XML_URL       = 'https://github.com/hugo19941994/auto-datfile-generator/releases/latest/download/redump.zip'
+URL_HOME      = "http://redump.org/"
+URL_DOWNLOADS = "http://redump.org/downloads/"
+XML_FILENAME  = "redump.xml"
+XML_URL       = "https://github.com/hugo19941994/auto-datfile-generator/releases/latest/download/redump.zip"
 
 regex = {
-    'datfile': r'<a href="/datfile/(.*?)">',
-    'date': r'\) \((.*?)\)\.',
-    'name': r'filename="(.*?) Datfile',
-    'filename': r'filename="(.*?)"',
+    "datfile"  : r'<a href="/datfile/(.*?)">',
+    "date"     : r"\) \((.*?)\)\.",
+    "name"     : r'filename="(.*?) Datfile',
+    "filename" : r'filename="(.*?)"',
 }
 
 
@@ -23,7 +23,7 @@ def _find_dats():
     download_page = requests.get(URL_DOWNLOADS, timeout=30)
     download_page.raise_for_status()
 
-    dat_files = re.findall(regex['datfile'], download_page.text)
+    dat_files = re.findall(regex["datfile"], download_page.text)
     return dat_files
 
 
@@ -31,52 +31,51 @@ def update_XML():
     dat_list = _find_dats()
 
     # zip file to store all DAT files
-    zip_object = zipfile.ZipFile('redump.zip', 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=9)
+    zip_object = zipfile.ZipFile("redump.zip", "w", compression=zipfile.ZIP_DEFLATED, compresslevel=9)
 
     # clrmamepro XML file
-    tag_clrmamepro = ET.Element('clrmamepro')
+    tag_clrmamepro = ET.Element("clrmamepro")
 
     for dat in dat_list:
-        print(f'Downloading {dat}')
+        print(f"Downloading {dat}")
         # section for this dat in the XML file
-        tag_datfile = ET.SubElement(tag_clrmamepro, 'datfile')
+        tag_datfile = ET.SubElement(tag_clrmamepro, "datfile")
 
-        response = requests.get(URL_HOME+'datfile/'+dat, timeout=30)
-        content_header = response.headers['Content-Disposition']
+        response = requests.get(URL_HOME + "datfile/" + dat, timeout=30)
+        content_header = response.headers["Content-Disposition"]
 
         # XML version
-        dat_date = re.findall(regex['date'], content_header)[0]
-        ET.SubElement(tag_datfile, 'version').text = dat_date
+        dat_date = re.findall(regex["date"], content_header)[0]
+        ET.SubElement(tag_datfile, "version").text = dat_date
 
         # XML name & description
-        temp_name = re.findall(regex['name'], content_header)[0]
+        temp_name = re.findall(regex["name"], content_header)[0]
         # trim the - from the end (if exists)
-        if temp_name.endswith('-'):
+        if temp_name.endswith("-"):
             temp_name = temp_name[:-2]
-        elif temp_name.endswith('BIOS'):
-            temp_name = temp_name + ' Images'
-        ET.SubElement(tag_datfile, 'name').text = temp_name
-        ET.SubElement(tag_datfile, 'description').text = temp_name
+        elif temp_name.endswith("BIOS"):
+            temp_name = temp_name + " Images"
+        ET.SubElement(tag_datfile, "name").text = temp_name
+        ET.SubElement(tag_datfile, "description").text = temp_name
 
         # URL tag in XML
-        ET.SubElement(
-            tag_datfile, 'url').text = XML_URL
+        ET.SubElement(tag_datfile, "url").text = XML_URL
 
         # File tag in XML
-        original_filename = re.findall(regex['filename'], content_header)[0]
-        filename = f'{original_filename[:-4]}.dat'
-        ET.SubElement(tag_datfile, 'file').text = filename
+        original_filename = re.findall(regex["filename"], content_header)[0]
+        filename = f"{original_filename[:-4]}.dat"
+        ET.SubElement(tag_datfile, "file").text = filename
 
         # Author tag in XML
-        ET.SubElement(tag_datfile, 'author').text = 'redump.org'
+        ET.SubElement(tag_datfile, "author").text = "redump.org"
 
         # Command XML tag
-        ET.SubElement(tag_datfile, 'comment').text = '_'
+        ET.SubElement(tag_datfile, "comment").text = "_"
 
         # Get the DAT file
-        datfile_name = f'{filename[:-4]}.dat'
-        print(f'DAT filename: {datfile_name}')
-        if original_filename.endswith('.zip'):
+        datfile_name = f"{filename[:-4]}.dat"
+        print(f"DAT filename: {datfile_name}")
+        if original_filename.endswith(".zip"):
             # extract datfile from zip to store in the DB zip
             zipdata = BytesIO()
             zipdata.write(response.content)
@@ -92,10 +91,10 @@ def update_XML():
     # store clrmamepro XML file
     xmldata = ET.tostring(tag_clrmamepro).decode()
 
-    with open(XML_FILENAME, 'w', encoding="utf-8") as xmlfile:
+    with open(XML_FILENAME, "w", encoding="utf-8") as xmlfile:
         xmlfile.write(xmldata)
 
-    print('Finished')
+    print("Finished")
 
 
 try:
